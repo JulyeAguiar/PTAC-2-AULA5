bd90c5511b98230b81ddade80277152d8e9dd75a
JulyeAguiar <131816932+JulyeAguiar@users.noreply.github.com>
2023-08-31 22:11:11 -0300

    pj

--- src/Home/Home.jsx
+++ src/Home/Home.jsx
@@ -1,11 +1,33 @@
 import { useState } from "react";
 import { Link } from "react-router-dom";
 export default function Home() {
-   
+   const [atividades, setAtividades] = useState("")
+   const [listas, setListas] = useState([])
+   const [identidade, setIdentidade] = useState(1)
+   const salvar = (e) =>{
+    e.preventDefault()
+    setListas([...listas,{
+        atividades:atividades, identidade:identidade
+    }])
+    setIdentidade(identidade + 1)
+    alert("A palavra salva é... " + atividades)
+    console.log(listas)
+   }
     return (
         <div>
-            <h1>Home</h1>
             <Link to="/todo">todo</Link>
+            <h1>Lista de Atividade</h1>
+            <form onSubmit={salvar}>
+                <input type="text" onChange={(e) => {setAtividades(e.target.value)}}></input>
+                <button>Butão</button>
+                
+            </form>
+
+            {listas.map((atv) =>
+                <div key={atv.identidade}>
+                    <p>{atv.atividades}</p>
+                </div>
+            )}
         </div>
 
     );
